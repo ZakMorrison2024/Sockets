ad5e1cf08a62574e8d412d10bfbbbc962778a016
Zak Morrison <146549624+ZakMorrison2024@users.noreply.github.com>
2024-12-16 12:39:31 +0000

    Create Python_Socket_Client_Var.py

---
+++ Python_Socket_Client_Var.py
@@ -0,0 +1,6 @@
+Multiplayer = False # Network mode
+player_name = "" # Network alias
+Client = False # Client Mode
+online_host_address = "" # Server IP address
+online_host_port = "" # Server PORT number
+logs = [] # logs
