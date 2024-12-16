da2785a125e9abbd55cefb818f2ecd2536cca812
Zak Morrison <146549624+ZakMorrison2024@users.noreply.github.com>
2024-12-16 12:49:33 +0000

    Create Python_Socket_Client_Recieve_Message.py

---
+++ Python_Socket_Client_Recieve_Message.py
@@ -0,0 +1,18 @@
+def recv_message(message): # recieved message
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: # establish connection
+            sock.setblocking(0) # stop blocking sockets
+            readable, writable, errored = select.select([sock], [], [], 0) # is readable buffer
+            if readable:
+                message = sock.recv(1024).decode() # read message
+              
+                if message == str: # if message is a string
+                    return see_message(message) # read message
+                elif message == int: # if message is a number
+                    network_action(int(message))
+                  
+    except Exception as e:
+       send_message(My_IP + " | 404: Didn't get any message.")
+       return "Error: Unable to recieve data from server."
+    except BlockingIOError:
+       sock.setblocking(0)# stop blocking
