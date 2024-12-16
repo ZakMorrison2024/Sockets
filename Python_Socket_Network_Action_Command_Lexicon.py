1b2ff7d8df2228e5df1af7ec3c2381e0318cdd17
Zak Morrison <146549624+ZakMorrison2024@users.noreply.github.com>
2024-12-16 12:51:05 +0000

    Create Python_Socket_Network_Action_Command_Lexicon.py

---
+++ Python_Socket_Network_Action_Command_Lexicon.py
@@ -0,0 +1,8 @@
+def network_action(message):  # This is a int command line, using the messaging system.
+    if message == int:
+        if message == 1:
+           # do something
+           pass
+        if message == 2:
+            # do something
+           pass 
